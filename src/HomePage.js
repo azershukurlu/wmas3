93f9e48a665644c9911c602d983862b6d2c77f88
Azer Shukurlu <ashukurlu12315@ada.edu.az>
2023-12-30 02:16:49 +0400

    Initial commit

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="main-container">
+      <h1>Greetings from My Digital Space!</h1>
+      <p></p>
+      <ul className="project-container"></ul>
+    </div>
+  );
+}
+
+export default HomePage;
