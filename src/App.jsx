862f3a41474e4ad02d8b5ed70c0332fe12ff0d03
Azer Shukurlu <ashukurlu12315@ada.edu.az>
2023-12-30 09:59:12 +0400

    Add css for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "./HomePage";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./style/App.css";
 
 function App() {
   return (
